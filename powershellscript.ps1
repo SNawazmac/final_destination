de80dfbb9b84c8f0ecb1205cd1709475eb22a414
S Nawaz <95207462+SNawazmac@users.noreply.github.com>
2023-04-12 23:30:14 +0530

    Update powershellscript.ps1

--- powershellscript.ps1
+++ powershellscript.ps1
@@ -9,7 +9,8 @@ $Credential = New-Object -TypeName System.Management.Automation.PSCredential -Ar
 Connect-AzAccount -ServicePrincipal -TenantId "36da45f1-dd2c-4d1f-af13-5abe46b99921" -Credential $Credential
 Set-AzContext -Subscription "8468b937-7938-4c80-8bce-8ddbdc17364c"
 
-foreach($storageaccount_name in $storageaccount_names)
+$storageaccounts = $storageaccount_names.split(",")
+foreach($storageaccount_name in $storageaccounts)
 {
 #Below command Invokes failover on the selected storage account(s)
 $failover = Invoke-AzStorageAccountFailover -ResourceGroupName $resource_group_name -Name $storageaccount_name -Force -AsJob
