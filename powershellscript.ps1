d02314db7c47fbcf3652e4fdb66682ac66157ae5
S Nawaz <95207462+SNawazmac@users.noreply.github.com>
2023-04-12 21:11:41 +0530

    Update powershellscript.ps1

--- powershellscript.ps1
+++ powershellscript.ps1
@@ -1,8 +1,9 @@
 param(
 [Parameter(Mandatory=$true)][string]$resource_group_name,       #Enter the resourcegroup name of the Storageaccount
 [Parameter(Mandatory=$true)][array]$storageaccount_names        #Enter the storage account name(s) on which failover has to be initiated
-#[Parameter(Mandatory=$true)][string]$sku                       #Enter the SKU as Standard_GRS to re-enable geo-replication post failover
 )
+#[Parameter(Mandatory=$true)][string]$sku                       #Enter the SKU as Standard_GRS to re-enable geo-replication post failover
+
 
 $securePassword = ConvertTo-SecureString "fLz8Q~8BA.M5xmcBfagAh1fnYdDnSnzbZ.ZTic6q" -AsPlainText -Force
 $Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList "17f6ad18-c18b-4ba4-9539-73ef8db53a1e",$securePassword
@@ -12,7 +13,7 @@ Set-AzContext -Subscription "8468b937-7938-4c80-8bce-8ddbdc17364c"
 foreach($storageaccount_name in $storageaccount_names)
 {
 #Below command Invokes failover on the selected storage account(s)
-$failover = Invoke-AzStorageAccountFailover -ResourceGroupName $resource_group_name -Name $storageaccount_name -Force -AsJob
+Invoke-AzStorageAccountFailover -ResourceGroupName $resource_group_name -Name $storageaccount_name -Force -AsJob
 }
 
 #Wait-Job $failover
