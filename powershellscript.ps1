443202d8d752f8972c09a5a01053a8d5ca56ad62
S Nawaz <95207462+SNawazmac@users.noreply.github.com>
2023-04-13 08:47:43 +0530

    Update powershellscript.ps1

--- powershellscript.ps1
+++ powershellscript.ps1
@@ -10,13 +10,13 @@ Connect-AzAccount -ServicePrincipal -TenantId "36da45f1-dd2c-4d1f-af13-5abe46b99
 Set-AzContext -Subscription "8468b937-7938-4c80-8bce-8ddbdc17364c"
 
 $storageaccounts = $storageaccount_names.split(",")
-foreach($storageaccount_name in $storageaccounts)
-{
+#foreach($storageaccount_name in $storageaccounts)
+#{
 #Below command Invokes failover on the selected storage account(s)
-$failover = Invoke-AzStorageAccountFailover -ResourceGroupName $resource_group_name -Name $storageaccount_name -Force -AsJob
-}
+#$failover = Invoke-AzStorageAccountFailover -ResourceGroupName $resource_group_name -Name $storageaccount_name -Force -AsJob
+#}
 
-Wait-Job $failover
+#Wait-Job $failover
 
 foreach($storageaccount_name in $storageaccounts)
 {
