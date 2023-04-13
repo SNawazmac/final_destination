41f5e1e7244cf674b559536c0e0e3d78d6ec9345
S Nawaz <95207462+SNawazmac@users.noreply.github.com>
2023-04-13 08:56:05 +0530

    Update powershellscript.ps1

--- powershellscript.ps1
+++ powershellscript.ps1
@@ -9,16 +9,16 @@ $Credential = New-Object -TypeName System.Management.Automation.PSCredential -Ar
 Connect-AzAccount -ServicePrincipal -TenantId "36da45f1-dd2c-4d1f-af13-5abe46b99921" -Credential $Credential
 Set-AzContext -Subscription "8468b937-7938-4c80-8bce-8ddbdc17364c"
 
-$storageaccounts = $storageaccount_names.split(",")
-#foreach($storageaccount_name in $storageaccounts)
-#{
+#$storageaccounts = $storageaccount_names.split(",")
+foreach($storageaccount_name in $storageaccount_names)
+{
 #Below command Invokes failover on the selected storage account(s)
-#$failover = Invoke-AzStorageAccountFailover -ResourceGroupName $resource_group_name -Name $storageaccount_name -Force -AsJob
-#}
+$failover = Invoke-AzStorageAccountFailover -ResourceGroupName $resource_group_name -Name $storageaccount_name -Force -AsJob
+}
 
-#Wait-Job $failover
+Wait-Job $failover
 
-foreach($storageaccount_name in $storageaccounts)
+foreach($storageaccount_name in $storageaccount_names)
 {
 #Below command updates the SKU of the storage account(s) to Standard_GRS post failover
 Set-AzStorageAccount -ResourceGroupName $resource_group_name -Name $storageaccount_name -SkuName $sku -Force
